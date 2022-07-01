60983019b4797731d93860bfc49368b2a802cc90
Ben Kiboma <omayioben@gmail.com>
2022-07-01 10:27:48 +0300

    Increment function

--- src/App.js
+++ src/App.js
@@ -8,11 +8,15 @@ function App() {
     setCount(count - 1);
   }
 
+  function incrementCount() {
+    setCount(count + 1);
+  }
+
   return (
     <div className='App'>
       <button className='btn' onClick={decrementCount} >-</button>
       <span>{count}</span>
-      <button className='btn'>+</button>
+      <button className='btn' onClick={incrementCount}>+</button>
     </div>
   );
 }
